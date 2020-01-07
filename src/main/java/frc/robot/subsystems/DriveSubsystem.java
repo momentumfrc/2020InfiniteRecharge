40e158e29479847583b84ff3589aaed0fc8c6797
FireShredder24 <knightofthealtar64@gmail.com>
2020-01-07 15:31:26 -0800

    Follow() Falcons in drive pairs
    Use WPI_TalonFX instead of TalonFX

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,22 +9,27 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.*;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
-
-  private final TalonFX leftFront = new TalonFX(FALCON_DRIVE_LEFT_FRONT_CAN_ADDR);
-  private final TalonFX leftRear = new TalonFX(FALCON_DRIVE_LEFT_REAR_CAN_ADDR);
-  private final TalonFX rightFront = new TalonFX(FALCON_DRIVE_RIGHT_FRONT_CAN_ADDR);
-  private final TalonFX rightRear = new TalonFX(FALCON_DRIVE_RIGHT_REAR_CAN_ADDR);
+  // Need to use WPI_TalonFX so that DifferentialDrive will accept the motors.
+  private final WPI_TalonFX leftFront = new WPI_TalonFX(FALCON_DRIVE_LEFT_FRONT_CAN_ADDR);
+  private final WPI_TalonFX leftRear = new WPI_TalonFX(FALCON_DRIVE_LEFT_REAR_CAN_ADDR);
+  private final WPI_TalonFX rightFront = new WPI_TalonFX(FALCON_DRIVE_RIGHT_FRONT_CAN_ADDR);
+  private final WPI_TalonFX rightRear = new WPI_TalonFX(FALCON_DRIVE_RIGHT_REAR_CAN_ADDR);
+  private final DifferentialDrive drive;
 
   /**
    * Creates a new ExampleSubsystem.
    */
   public DriveSubsystem() {
-    // TODO: combine Talons into master/slave groups.
+    leftRear.follow(leftFront);
+    rightRear.follow(rightFront);
+
+    drive = new DifferentialDrive(leftFront, rightFront);
   }
 
   @Override
