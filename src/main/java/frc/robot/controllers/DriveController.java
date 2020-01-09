33670689d29d6241518ddc892b6af2bb42e27f69
John Nguyen <knightofthealtar64@gmail.com>
2020-01-08 21:03:33 -0800

    Create DriveController interface
    Complete with all control-distinct items from the priority list.

---
+++ src/main/java/frc/robot/controllers/DriveController.java
@@ -0,0 +1,73 @@
+package frc.robot.controllers;
+
+public interface DriveController {
+  /**
+   * Gets the current speed the robot should move at.
+   * 
+   * @return the requested move speed.
+   */
+  abstract public double getMoveRequest();
+
+  /**
+   * Gets the current rotation the robot should turn to.
+   * 
+   * @return the requested turn magnitude.
+   */
+  abstract public double getTurnRequest();
+
+  /**
+   * Gets the maximum speed the robot should drive at.
+   * 
+   * @return the speed limit.
+   */
+  abstract public double getSpeedLimiter();
+
+  /**
+   * Gets whether the robot should invert drive directions.
+   * 
+   * @return drive inversion boolean.
+   */
+  abstract public boolean getReverseDirection();
+
+  /**
+   * Gets whether the robot should intake power cells.
+   * 
+   * @return whether the robot should intake power cells.
+   */
+  abstract public boolean getIntakePowerCells();
+
+  /**
+   * Gets whether the robot should shoot power cells at the lower port.
+   * 
+   * @return whether the robot should shoot power cells at the lower port.
+   */
+  abstract public boolean getShootPowerCellsLow();
+
+  /**
+   * Gets whether the robot should shoot power cells at the outer port.
+   * 
+   * @return whether the robot should shoot power cells at the outer port.
+   */
+  abstract public boolean getShootPowerCellsOuter();
+
+  /**
+   * Gets whether the robot should shoot power cells at the inner port.
+   * 
+   * @return whether the robot should shoot power cells at the inner port.
+   */
+  abstract public boolean getShootPowerCellsInner();
+
+  /**
+   * Gets whether the robot should attempt the Rotation Control objective.
+   * 
+   * @return whether the robot should attempt the Rotation Control objective.
+   */
+  abstract public boolean getRotationCtrl();
+
+  /**
+   * Gets whether the robot should attempt the Position Control objective.
+   * 
+   * @return whether the robot should attempt the Position Control objective.
+   */
+  abstract public boolean getPositionCtrl();
+}
