7d56f60e53a86fbf8191f3c6b0daf75d51223958
John Nguyen <37886033+FireShredder24@users.noreply.github.com>
2020-02-21 10:50:58 -0800

    Create ShooterHoodSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/ShooterHoodSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import com.REVRobotics.CANSparkMAX;
+import edu.wpi.first.wpilibj2.SubsystemBase;
+
+public class ShooterHoodSubsystem extends SubsystemBase {
+  
+  public ShooterHoodSubsystem() {
+  
+  }
+}
