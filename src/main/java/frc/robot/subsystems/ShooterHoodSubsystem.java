71ae020d73ffd9893f6df6191ebccb46bd2447f8
John Nguyen <37886033+FireShredder24@users.noreply.github.com>
2020-02-21 10:55:49 -0800

    Add moveHood() with a variable moveRequest

--- src/main/java/frc/robot/subsystems/ShooterHoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterHoodSubsystem.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems;
 
-import com.REVRobotics.CANSparkMAX;
+import com.REVRobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.SubsystemBase;
 
 public class ShooterHoodSubsystem extends SubsystemBase {
-  
+  private final CANSparkMax hoodNEO;
   public ShooterHoodSubsystem() {
-  
+    
+  }
+  public void moveHood(double moveRequest) {
+     
   }
 }
