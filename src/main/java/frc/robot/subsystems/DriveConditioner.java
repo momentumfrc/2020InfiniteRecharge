7a28d4b74cc66f535ee06d97a2aedb713a5c179e
FireShredder24 <knightofthealtar64@gmail.com>
2020-03-04 18:26:16 -0800

    Cleanup speed limit inc/dec

--- src/main/java/frc/robot/subsystems/DriveConditioner.java
+++ src/main/java/frc/robot/subsystems/DriveConditioner.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import org.usfirst.frc.team4999.utils.Utils;
 
 public class DriveConditioner {
-  private double spd_limit;
   private final double[] spd_limits = { 0.25, 0.33333, 0.5, 0.66666, 0.75, 1 };
   private int spd_limit_index;
 
@@ -11,11 +10,8 @@ public class DriveConditioner {
     spd_limit_index = spd_limits.length - 1;
   }
 
-  public void setSpeedLimit(double limit) {
-    spd_limit = limit;
-  }
-
   public double conditionMove(double moveRequest) {
+    double spd_limit = spd_limits[spd_limit_index];
     return Utils.map(moveRequest, -1, 1, -spd_limit, spd_limit);
   }
 
@@ -24,18 +20,10 @@ public class DriveConditioner {
   }
 
   public void incSpeedLimit() {
-    ++spd_limit_index;
-    if (spd_limit_index >= spd_limits.length)
-      spd_limit_index = spd_limits.length - 1;
-    spd_limit = spd_limits[spd_limit_index];
+    spd_limit_index = Math.min(spd_limit_index + 1, spd_limits.length - 1);
   }
 
   public void decSpeedLimit() {
     spd_limit_index = Math.max(spd_limit_index - 1, 0);
-    spd_limit = spd_limits[spd_limit_index];
-  }
-
-  public void stop() {
-
   }
 }
