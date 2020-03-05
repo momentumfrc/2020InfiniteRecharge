132f796d1a9f09771bf29650a94d7ef5f41a6bc7
Scot Woodyard <scot.woodyard@gmail.com>
2020-03-05 09:30:44 -0800

    Added StorageSubsystem  code from 3/4 commit
     code from 3/4 commit

--- src/main/java/frc/robot/subsystems/StorageSubsystem.java
+++ src/main/java/frc/robot/subsystems/StorageSubsystem.java
@@ -2,29 +2,32 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.VictorSP;
+import frc.robot.Constants;
+import frc.robot.utils.MoPrefs;
 
 public class StorageSubsystem extends SubsystemBase {
-  private final VictorSP storage = new VictorSP(Constants.STORAGE_VICTORSP_PWM_CHAN);
-  
-  
+  private final VictorSP storage;
+  private double speed;
+
   public StorageSubsystem() {
-  
+    storage = new VictorSP(Constants.STORAGE_VICTORSP_PWM_CHAN);
+    stop();
   }
-  
-  public void runStorage() {
-    
+
+  public void run() {
+    speed = MoPrefs.getStorageSpeed();
   }
-  
-  public void stopStorage() {
-    
+
+  public void reverse() {
+    speed = -MoPrefs.getStorageSpeed();
   }
-  
-  public void purge() {
-    
+
+  public void stop() {
+    speed = 0;
   }
-  
+
   @Override
   public void periodic() {
-    
+    storage.set(speed);
   }
 }
