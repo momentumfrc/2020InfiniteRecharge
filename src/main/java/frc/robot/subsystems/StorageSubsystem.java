0e555b649c6c5023b6692b90bbcd1c24468d7490
John Nguyen <37886033+FireShredder24@users.noreply.github.com>
2020-03-05 09:15:29 -0800

    Create empty methods

--- src/main/java/frc/robot/subsystems/StorageSubsystem.java
+++ src/main/java/frc/robot/subsystems/StorageSubsystem.java
@@ -1,7 +1,30 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.VictorSP;
 
 public class StorageSubsystem extends SubsystemBase {
-
+  private final VictorSP storage = new VictorSP(Constants.STORAGE_VICTORSP_PWM_CHAN);
+  
+  
+  public StorageSubsystem() {
+  
+  }
+  
+  public void runStorage() {
+    
+  }
+  
+  public void stopStorage() {
+    
+  }
+  
+  public void purge() {
+    
+  }
+  
+  @Override
+  public void periodic() {
+    
+  }
 }
