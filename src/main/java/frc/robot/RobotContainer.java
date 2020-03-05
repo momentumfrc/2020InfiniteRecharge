126778c1db2a1790af332bfc5181ba5ba7a405f6
Jordan Powers <jordanpowers1227@gmail.com>
2020-03-05 00:24:15 -0800

    Replace default InstantCommands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,7 @@ import frc.robot.controllers.ControllerBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
@@ -79,9 +80,9 @@ public class RobotContainer {
     configureButtonBindings();
 
     // Set default commands as needed
-    intakeSubsystem.setDefaultCommand(new InstantCommand(intakeSubsystem::idle, intakeSubsystem));
-    climberSubsystem.setDefaultCommand(new InstantCommand(climberSubsystem::stop, climberSubsystem));
-    shooterSubsystem.setDefaultCommand(new InstantCommand(shooterSubsystem::idle, shooterSubsystem));
+    intakeSubsystem.setDefaultCommand(new RunCommand(intakeSubsystem::idle, intakeSubsystem));
+    climberSubsystem.setDefaultCommand(new RunCommand(climberSubsystem::stop, climberSubsystem));
+    shooterSubsystem.setDefaultCommand(new RunCommand(shooterSubsystem::idle, shooterSubsystem, shooterHoodSubsystem));
   }
 
   /**
