4757567c8e71181786e6ea17848cafc64582ab12
Jordan Powers <jordanpowers1227@gmail.com>
2020-04-23 14:58:20 -0700

    Add DriveCommand test

---
+++ src/main/java/frc/robot/subsystems/conditioners/PassthroughConditioner.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.conditioners;
+
+public class PassthroughConditioner implements DriveConditioner {
+
+  @Override
+  public double conditionMove(double moveRequest) {
+    return moveRequest;
+  }
+
+  @Override
+  public double conditionTurn(double turnRequest) {
+    return turnRequest;
+  }
+
+}
