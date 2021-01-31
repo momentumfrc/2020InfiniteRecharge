c9a4cfe9b09fc03e3af6a01447bc381aff2258d8
John Nguyen <knightofthealtar64@gmail.com>
2021-01-31 12:32:45 -0800

    Fetch trajectory paths

--- src/main/java/frc/robot/commands/PathWeaverCommand.java
+++ src/main/java/frc/robot/commands/PathWeaverCommand.java
@@ -1,9 +1,22 @@
 package frc.robot.commands;
 
+import java.io.IOException;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class PathWeaverCommand extends CommandBase {
-  public PathWeaverCommand() {
+  private static final String TRAJECTORY_PATH = "paths/test.path";
+  private Trajectory trajectory;
 
+  public PathWeaverCommand() {
+    try {
+      trajectory = TrajectoryUtil.fromPathweaverJson(Filesystem.getDeployDirectory().toPath().resolve(TRAJECTORY_PATH));
+    } catch (IOException e) {
+      DriverStation.reportError("Unable to open trajectory: " + TRAJECTORY_PATH, e.getStackTrace());
+    }
   }
 }
