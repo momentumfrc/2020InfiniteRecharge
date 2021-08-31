9ece670e61a39743cc13435559d2cf7e73647cfd
Jordan Powers <jordanpowers1227@gmail.com>
2021-08-30 20:24:29 -0700

    Add parameterized constructor to DeadzoneConditioner

--- src/main/java/frc/robot/subsystems/conditioners/DeadzoneConditioner.java
+++ src/main/java/frc/robot/subsystems/conditioners/DeadzoneConditioner.java
@@ -3,8 +3,21 @@ package frc.robot.subsystems.conditioners;
 import org.usfirst.frc.team4999.utils.Utils;
 
 public class DeadzoneConditioner implements DriveConditioner {
-  private final double move_request_deadzone = 0.1;
-  private final double turn_request_deadzone = 0.1;
+  private static final double DEFAULT_MOVE_REQUEST_DEADZONE = 0.1;
+  private static final double DEFAULT_TURN_REQUEST_DEADZONE = 0.1;
+
+  private final double move_request_deadzone;
+  private final double turn_request_deadzone;
+
+  public DeadzoneConditioner() {
+    move_request_deadzone = DEFAULT_MOVE_REQUEST_DEADZONE;
+    turn_request_deadzone = DEFAULT_TURN_REQUEST_DEADZONE;
+  }
+
+  public DeadzoneConditioner(double moveRequestDeadzone, double turnRequestDeadzone) {
+    move_request_deadzone = moveRequestDeadzone;
+    turn_request_deadzone = turnRequestDeadzone;
+  }
 
   @Override
   public double conditionMove(double moveRequest) {
